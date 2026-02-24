43f3edb2970251fdcc75ba48c540ed5245850949
lordkelwin <elwin.tano14@gmail.com>
2026-02-24 19:37:44 +0800

    Added more details in the solution. Improved handling on lines.

--- problem_1.py
+++ problem_1.py
@@ -92,7 +92,7 @@ class VelocityProblemSolution(Scene):
             MathTex(r"t^2 - t + 1 = A(t^3 + 2t^2 + t + 2) + B(t^2 + 1) + \\ (Ct^3 + 4Ct^2 + 4Ct + Dt^2 + 4Dt + 4D)"),
             MathTex(r"t^2 - t + 1 = (At^3 + 2At^2 + At + 2A) + (Bt^2 + B) + \\ (Ct^3 + 4Ct^2 + 4Ct)  + "
                     r"(Dt^2 + 4Dt + 4D)"),
-            MathTex(r"t^2 - t + 1 =(A + C)t^3 + (2A + B + 4C + D)t^2 + \\ (A + B + 4C + 4D)t + (2A + B + 4D)")
+            MathTex(r"t^2 - t + 1 =(A + C)t^3 + (2A + B + 4C + D)t^2 + \\ (A + 4C + 4D)t + (2A + B + 4D)")
         ]
 
         lines.arrange(DOWN, buff=MED_LARGE_BUFF)
@@ -116,7 +116,7 @@ class VelocityProblemSolution(Scene):
             Tex("Equating the coefficients in both sides:"),
             MathTex(r"t^3:\,A + C = 0\,\,\,\,\mathrm{(eq. 1)}"),
             MathTex(r"t^2:\,2A + B + 4C + D = 1\,\,\,\mathrm{(eq. 2)}"),
-            MathTex(r"t:\,A + B + 4C + 4D = -1\,\,\,\mathrm{(eq. 3)}"),
+            MathTex(r"t:\,A + 4C + 4D = -1\,\,\,\mathrm{(eq. 3)}"),
             MathTex(r"t^0:\,2A + B + 4D = 1\,\,\,\mathrm{(eq. 4)}")
         )
 
@@ -136,19 +136,19 @@ class VelocityProblemSolution(Scene):
             MathTex(r"A + C = 0"),
             Tex("Substitute equation 5 in equations 2, 3, and 4:"),
             MathTex(r"A + 2B + 4C + D = 1"),
-            MathTex(r"A + B + 4C + 4D = -1"),
+            MathTex(r"A + 4C + 4D = -1"),
             MathTex(r"2A + B + 4D = 1")
         )
 
         lines_extra = VGroup(
             MathTex(r"A = -C"),
             MathTex(r"A = -C\,\,\,\mathrm{(eq. 5)}"),
-            MathTex(r"-C + 2B + 4C + D = 1"),
-            MathTex(r"2B + 3C + D = 1\,\,\,\mathrm{(eq. 6)}"),
-            MathTex(r"-C + B + 4C + 4D = -1"),
-            MathTex(r"B + 3C + 4D = -1\,\,\,\mathrm{(eq. 7)}"),
+            MathTex(r"-2C + B + 4C + D = 1"),
+            MathTex(r"B + 2C + D = 1\,\,\,\mathrm{(eq. 6)}"),
+            MathTex(r"-C + 4C + 4D = -1"),
+            MathTex(r"3C + 4D = -1\,\,\,\mathrm{(eq. 7)}"),
             MathTex(r"2(-C) + B + 4D = 1"),
-            MathTex(r"-2C + B + 4D = 1\,\,\,\mathrm{(eq. 8)}")
+            MathTex(r"B - 2C + 4D = 1\,\,\,\mathrm{(eq. 8)}")
         )
 
         lines.arrange(DOWN, buff=MED_LARGE_BUFF)
@@ -187,3 +187,104 @@ class VelocityProblemSolution(Scene):
         self.wait(2)
         self.play(FadeOut(lines[0], lines[2], lines_extra[1:8:2]))
 
+        lines = VGroup(
+            Tex("Subtracting equations 6 and 8:"),
+            MathTex(r"(B + 2C + D) - (B - 2C + 4D) = 1 - 1"),
+            Tex("Substitute equation 9 in equation 7:"),
+            MathTex(r"3C + 4D = -1")
+        )
+
+        lines_extra = [
+            MathTex(r"B + 2C + D - B + 2C - 4D = -1 - 1"),
+            MathTex(r"4C - 3D = 0"),
+            MathTex(r"4C = 3D"),
+            MathTex(r"\frac{1}{4}\times 4C = 3D \times \frac{1}{4}"),
+            MathTex(r"C = \frac{3}{4} D\,\,\, \mathrm{(eq. 9)}"),
+            MathTex(r"3(\frac{3}{4}D) + 4D = -1"),
+            MathTex(r"\frac{9}{4}D + 4D = -1"),
+            MathTex(r"4\times\bigg(\frac{9}{4}D + 4D\bigg) = -1 \times 4"),
+            MathTex(r"9D + 16D = -4"),
+            MathTex(r"25D = -4"),
+            MathTex(r"\frac{1}{25}\times 25D = -4 \times \frac{1}{25}"),
+            MathTex(r"D = -\frac{4}{25}")
+        ]
+
+        lines.arrange(DOWN, buff=MED_LARGE_BUFF)
+
+        self.add(lines[0])
+        self.play(Write(lines[0]))
+        self.wait()
+        self.play(Write(lines[1]))
+        self.wait(0.5)
+        self.play(ReplacementTransform(lines[1], lines_extra[0].next_to(lines[0], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(ReplacementTransform(lines_extra[0].next_to(lines[0], direction=DOWN, buff=MED_LARGE_BUFF),
+                                       lines_extra[1].next_to(lines[0], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(ReplacementTransform(lines_extra[1].next_to(lines[0], direction=DOWN, buff=MED_LARGE_BUFF),
+                                       lines_extra[2].next_to(lines[0], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(ReplacementTransform(lines_extra[2].next_to(lines[0], direction=DOWN, buff=MED_LARGE_BUFF),
+                                       lines_extra[3].next_to(lines[0], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(ReplacementTransform(lines_extra[3].next_to(lines[0], direction=DOWN, buff=MED_LARGE_BUFF),
+                                       lines_extra[4].next_to(lines[0], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(Write(lines[2].next_to(lines_extra[4], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(Write(lines[3].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(ReplacementTransform(lines[3], lines_extra[5].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(ReplacementTransform(lines_extra[5].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF),
+                                       lines_extra[6].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(ReplacementTransform(lines_extra[6].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF),
+                                       lines_extra[7].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(ReplacementTransform(lines_extra[7].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF),
+                                       lines_extra[8].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(ReplacementTransform(lines_extra[8].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF),
+                                       lines_extra[9].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(ReplacementTransform(lines_extra[9].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF),
+                                       lines_extra[10].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(ReplacementTransform(lines_extra[10].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF),
+                                       lines_extra[11].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(1)
+        self.play(FadeOut(lines[0:3:2], lines_extra[4], lines_extra[11]))
+
+        lines = VGroup(
+            Tex("Substitute $D = -\\frac{4}{25}$ in equation 9:"),
+            MathTex(r"C = \frac{3}{4} \bigg(-\frac{4}{25}\bigg)"),
+            Tex("Substitute $C = -\\frac{3}{25}$ in equation 5:"),
+            MathTex(r"A= -\bigg(-\frac{3}{25}\bigg)"),
+        )
+
+        lines_extra = [
+            MathTex(r"C = -\frac{3}{25}"),
+            MathTex(r"A = \frac{3}{25}")
+        ]
+
+        lines.arrange(DOWN, buff=MED_LARGE_BUFF)
+
+        self.add(lines[0])
+        self.play(Write(lines[0]))
+        self.wait(0.5)
+        self.play(Write(lines[1]))
+        self.wait(0.5)
+        self.play(ReplacementTransform(lines[1], lines_extra[0].next_to(lines[0], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(Write(lines[2].next_to(lines_extra[0], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.5)
+        self.play(Write(lines[3]))
+        self.wait(0.5)
+        self.play(ReplacementTransform(lines[3], lines_extra[1].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(1)
+        self.play(FadeOut(lines[0:4:2], lines_extra[0], lines_extra[1]))
+
+        lines = VGroup(
+
+        )
