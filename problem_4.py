c6ea07c355b162a0e46666ab0ef9c91d0980ffa3
lordkelwin <elwin.tano14@gmail.com>
2026-03-03 17:21:30 +0800

    Code for the solution already finished, optimization especially is on-going.

--- problem_4.py
+++ problem_4.py
@@ -181,9 +181,9 @@ class ProblemSolution(Scene):
             MathTex(r"\mathbf{C}_{13}=(0)(2)-(s)(1)"),
             MathTex(r"\mathbf{C}_{13}=-s"),
             MathTex(r"\mathbf{C}_{21}=(-1)^{(2+1)} \begin{vmatrix} -1 & 0 \\ 2 & s+3 \end{vmatrix}"),
-            MathTex(r"\mathbf{C}_{21}=(-1)[(1)(s+3)-(2)(0)]"),
-            MathTex(r"\mathbf{C}_{21}=(-1)(s+3)"),
-            MathTex(r"\mathbf{C}_{21}=-(s+3)"),
+            MathTex(r"\mathbf{C}_{21}=(-1)[(-1)(s+3)-(2)(0)]"),
+            MathTex(r"\mathbf{C}_{21}=(-1)(-1)(s+3)"),
+            MathTex(r"\mathbf{C}_{21}=(s+3)"),
             MathTex(r"\mathbf{C}_{22}=(-1)^{(2+2)} \begin{vmatrix} s & 0 \\ 1 & s+3 \end{vmatrix}"),
             MathTex(r"\mathbf{C}_{22}=(s)(s+3)-(0)(1)"),
             MathTex(r"\mathbf{C}_{22}=s^{2}+3s"),
@@ -218,19 +218,20 @@ class ProblemSolution(Scene):
                                                                         buff=MED_LARGE_BUFF)))
         self.wait(0.3)
         self.play(ReplacementTransform(extra_lines[0], extra_lines[1].next_to(lines[2], direction=DOWN,
-                                                                        buff=MED_LARGE_BUFF)))
+                                                                              buff=MED_LARGE_BUFF)))
         self.wait(0.5)
         self.play(FadeOut(line_1, line_2, extra_lines[1]))
         self.wait(0.3)
         line_1 = Line(lines[2][0][7].get_center(), lines[2][0][10].get_center(), color=BLUE)
         line_2 = Line(lines[2][0][8:10].get_center(), lines[2][0][16].get_center(), color=BLUE)
-        self.play(Circumscribe(lines[2][0][8:10], shape=Circle, color=YELLOW, run_time=3, fade_out=True), Create(line_1),
+        self.play(Circumscribe(lines[2][0][8:10], shape=Circle, color=YELLOW, run_time=3, fade_out=True),
+                  Create(line_1),
                   Create(line_2))
         self.play(Write(extra_lines[2].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF)))
         self.wait(0.3)
         for i in range(3, 6):
-            self.play(ReplacementTransform(extra_lines[i-1], extra_lines[i].next_to(lines[2], direction=DOWN,
-                                                                                    buff=MED_LARGE_BUFF)))
+            self.play(ReplacementTransform(extra_lines[i - 1], extra_lines[i].next_to(lines[2], direction=DOWN,
+                                                                                      buff=MED_LARGE_BUFF)))
             self.wait(0.3)
         self.wait(0.2)
         self.play(FadeOut(line_1, line_2, extra_lines[5]))
@@ -311,4 +312,62 @@ class ProblemSolution(Scene):
             self.wait(0.3)
         self.wait(0.2)
         self.play(FadeOut(line_1, line_2, extra_lines[29], lines[0:3]))
+        self.wait(1)
+
+        lines = VGroup(
+            Tex("The Cofactor Matrix $C_{sI-A}$ is:"),
+            MathTex(r"C_{sI-A} = \begin{bmatrix} s^{2}+3s+2 & -1 & -s \\ s+3 & s^{2} + 3s & -(2s+1) \\"
+                    r" 1 & s & s^{2} \end{bmatrix}"),
+            Tex("The Adjoint Matrix $\\mathrm{adj}(sI-A)$:"),
+            MathTex(r"\mathrm{adj}(sI-A) = C_{sI-A}^{T}")
+        )
+
+        extra_lines = VGroup(
+            MathTex(r"\mathrm{adj}(sI-A) = \begin{bmatrix} s^{2}+3s+2 & -1 & -s \\ s+3 & s^{2} + 3s & -(2s+1) \\"
+                    r" 1 & s & s^{2} \end{bmatrix}^{T}"),
+            MathTex(r"\mathrm{adj}(sI-A)= \begin{bmatrix} s^{2}+3s+2 & s+3 & 1 \\ -1 & s^{2}+3s & s \\"
+                    r"-s & -(2s+1) & s^{2} \end{bmatrix}")
+        )
+
+        lines.arrange(DOWN, buff=MED_LARGE_BUFF)
+        self.add(lines[0])
+        for i in range(4):
+            self.play(Write(lines[i]))
+            self.wait(0.2)
+        self.play(ReplacementTransform(lines[3], extra_lines[0].next_to(lines[2], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.3)
+        self.play(ReplacementTransform(extra_lines[0], extra_lines[1].next_to(lines[2], direction=DOWN,
+                                                                              buff=MED_LARGE_BUFF)))
+        self.wait(1)
+        self.play(FadeOut(lines[:3], extra_lines[1]))
+
+        lines = VGroup(
+            Tex("Solving for the Phase-Variable Form:"),
+            MathTex(r"T(s) = C(sI-A)^{-1}B+D")
+        )
+
+        extra_lines = VGroup(
+            MathTex(r"T(s) = \frac{1}{s^{3}+3s^{2}+2s+1} \begin{bmatrix} 1 & 0 & 0 \end{bmatrix}"
+                    r"\begin{bmatrix} s^{2}+3s+2 & s+3 & 1 \\ -1 & s^{2}+3s & s \\"
+                    r"-s & -(2s+1) & s^{2} \end{bmatrix} \begin{bmatrix} 10 \\ 0 \\ 0 \end{bmatrix}"),
+            MathTex(r"T(s)=\frac{1}{s^{3}+3s^{2}+2s+1} \begin{bmatrix} s^{2}+3s+2 & s+3 & 1 \end{bmatrix}"
+                    r"\begin{bmatrix} 10 \\ 0 \\ 0 \end{bmatrix}"),
+            MathTex(r"T(s)=\frac{10(s^{2}+3s+2)}{s^{3}+3s^{2}+2s+1}")
+        )
+
+        lines.arrange(DOWN, buff=MED_LARGE_BUFF)
+        self.add(lines[0])
+
+        for i in range(2):
+            self.play(Write(lines[i]))
+            self.wait(0.3)
+        self.play(ReplacementTransform(lines[1], extra_lines[0].next_to(lines[0], direction=DOWN, buff=MED_LARGE_BUFF)))
+        self.wait(0.3)
+        for i in range(1, 3):
+            self.play(ReplacementTransform(extra_lines[i - 1],
+                                           extra_lines[i].next_to(lines[0], direction=DOWN, buff=MED_LARGE_BUFF)))
+            self.wait(0.3)
+        frameBox = SurroundingRectangle(extra_lines[2], buff=0.2)
+        self.play(Create(frameBox))
         self.wait(2)
+        self.play(FadeOut(lines[0], extra_lines[2], frameBox))
