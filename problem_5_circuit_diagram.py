aa487ccd5743ebf360ba3881cb68785925e9e717
lordkelwin <elwin.tano14@gmail.com>
2026-03-05 11:43:13 +0800

    Problem 5 Circuit Diagram completed.

--- problem_5_circuit_diagram.py
+++ problem_5_circuit_diagram.py
@@ -1,13 +1,12 @@
 import schemdraw
 import schemdraw.elements as elm
-import matplotlib
-matplotlib.use('Agg')
 
 
 with schemdraw.Drawing() as d:
-    elm.Inductor2().label(r'$0.5\,\mathrm{H}$')
-    elm.Resistor().down().label(r'$10\,\Omega$')
-    elm.Line().left()
-    elm.SourceSin().up().label(r'$3\sin{2t}\,\mathrm{V}$')
-    d.draw()
+    d.config(unit=5)
+    S1 = elm.SourceSin().label(r'$3\sin{2t}\,\mathrm{V}$', fontsize=20)
+    L1 = elm.Inductor2().right().label(r'$0.5\,\mathrm{H}$', fontsize=20)
+    R1 = elm.Resistor().down().label(r'$10\,\Omega$', fontsize=20, loc='bot')
+    line_1 = elm.Line().tox(S1.start)
+    elm.LoopCurrent([L1, R1, line_1, S1], pad=1).label(r'$i(t)$', fontsize=20)
     d.save('problem_5_circuit.svg')
