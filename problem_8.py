894e3513b1617eb9c7b47972d966a858c9fbed9a
lordkelwin <elwin.tano14@gmail.com>
2026-03-14 16:34:45 +0800

    Problem 8 - solution partially written

--- problem_8.py
+++ problem_8.py
@@ -27,5 +27,9 @@ class ProblemSolution(Scene):
         MathTex.set_default(font_size=90)
         solutionDetails = VGroup(
             Tex("Convolution theorem can be used when finding the inverse Laplace Transform"),
-            MathTex(r"")
+            MathTex(r"\mathcal{L}^{-1}\{F(s)G(s)\}={f(t)}\ast{g(t)}=\int_{0}^{t}f(x)g{t-x}\,dx"),
+            MathTex(r"\mathcal{L}^{-1}\bigg\{\frac{240}{(s^{2}+1)(s^{2}+25)\bigg\}=\mathcal{L}^{-1}\bigg\{48\times{"
+                    r"\frac{1}{s^{2}+1}}\times{\frac{5}{s^{2}+25}}\bigg\}"),
+            Tex("Where:"),
+            MathTex(r"F(s)=\frac{1}{s^{2}+1}\\G(s)=\frac{5}{s^{2}+25}")
         )
