37a9f295cfdf9bf49cd485c5670ea8a83b66924b
lordkelwin <elwin.tano14@gmail.com>
2026-03-17 19:43:10 +0800

    Problem 9 - problem statement done and solutions part will be started.

--- problem_9.py
+++ problem_9.py
@@ -4,5 +4,21 @@ from manim import *
 class problemStatement(Scene):
     def construct(self):
         statements = VGroup(
-
+            Tex(r"\begin{minipage}{6cm}"
+                r"A certain population is known to be growing at a rate given by the logistic equation "
+                r"$dx/dt=x(b-ax)$. Show that the maximum rate of growth will occur when the population is "
+                r"equal to half its equilibrium size, that is, when the population is $b/2a$."
+                r"\end{minipage}")
         )
+
+        statements.move_to(3 * UP)
+        self.play(Write(statements))
+        self.wait(3)
+        self.play(FadeOut(statements))
+
+
+class problemSolution(Scene):
+    def construct(self):
+        Tex.set_default(font_size=85)
+        MathTex.set(font_size=75)
+
