e9bc524898ea4646cc3370932961b6ef34611790
lordkelwin <elwin.tano14@gmail.com>
2026-03-17 20:05:01 +0800

    Problem 9 - solutions were already started, some solution steps were skipped and fixes will be implemented.

--- problem_9.py
+++ problem_9.py
@@ -7,8 +7,8 @@ class problemStatement(Scene):
             Tex(r"\begin{minipage}{6cm}"
                 r"A certain population is known to be growing at a rate given by the logistic equation "
                 r"$dx/dt=x(b-ax)$. Show that the maximum rate of growth will occur when the population is "
-                r"equal to half its equilibrium size, that is, when the population is $b/2a$."
-                r"\end{minipage}")
+                r"equal to half its equilibrium size, that is, when the \\population is $b/2a$."
+                r"\end{minipage}", font_size=80)
         )
 
         statements.move_to(3 * UP)
@@ -21,4 +21,17 @@ class problemSolution(Scene):
     def construct(self):
         Tex.set_default(font_size=85)
         MathTex.set(font_size=75)
+        solutionLines = VGroup(
+            Tex(r"\begin{minipage}{6 cm}"
+                r"To determine the equilibrium size, the growth rate must be equal to zero:"
+                r"\end{minipage}"),
+            MathTex(r"\frac{dx}{dt}=x(b-ax)"),
+            MathTex(r"\frac{dx}{dt}=bx-ax^{2}"),
+            Tex("Differentiate both sides with respect to $t$:"),
+            MathTex(r"\frac{d}{dt}\left[\frac{dx}{dt}=bx-ax^{2}\right]"),
+            MathTex(r"\frac{d^{2}x}{dt^{2}}=\frac{d}{dt}[bx-ax^{2}]"),
+            MathTex(r"\frac{d^{2}x}{dt^{2}}=b\frac{dx}{dt}-2ax\frac{dx}{dt}"),
+            Tex(r"For maximum rate of growth, the second derivative must be equal to zero:"),
+
+        )
 
