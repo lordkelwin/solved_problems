7cef6f9144cc77484ce7d76f0e343093d8a510ed
lordkelwin <elwin.tano14@gmail.com>
2026-03-21 23:13:00 +0800

    Problem 11 - solution is still on-going

--- problem_11.py
+++ problem_11.py
@@ -23,3 +23,31 @@ class ProblemStatement(Scene):
         self.wait(3)
         self.play(FadeOut(statement))
 
+
+class ProblemSolution(Scene):
+    def construct(self):
+        MathTex.set_default(font_size=75)
+        Tex.set_default(font_size=80)
+        solution = VGroup(
+            Tex(r"\begin{minipage}{5cm}"
+                r"It was stated that the rate of change with respect to time of the number of infected student $I$ is"
+                r"proportional to the number of uninfected students, $100-I$:"
+                r"\end{minipage}"),
+            MathTex(r"\frac{dI}{dt}=kI(100-I)"),
+            MathTex(r"\frac{1}{(dt)(I)(100-I)} \times \frac{dI}{dt} = kI(100-I) \times \frac{1}{(dt)(I)(100-I)}"),
+            MathTex(r"\frac{dI}{I(100-I)}=k\:dt"),
+            Tex(r"Integrating both sides:"),
+            MathTex(r"\int \left[\frac{dI}{I(100-I)}=k\:dt\right]"),
+            MathTex(r"\int \frac{dI}{I(100-I)} = \int k\:dt"),
+            Tex(r"Solving for the partial fraction decomposition:"),
+            MathTex(r"\frac{1}{I(100-I)}=\frac{A}{I}+\frac{B}{100-I}"),
+            Tex(r"\begin{minipage}{5.5cm}"
+                r"To solve for the coefficients, Heaviside Method is used:"
+                r"\end{minipage}"),
+            MathTex(r"A = \lim_{I->0} \frac{1}{100-I}"),
+            MathTex(r"A = \frac{1}{100-0}"),
+            MathTex(r"A = \frac{1}{100}"),
+            MathTex(r"B = \lim_{I->100} \frac{1}{I}"),
+            MathTex(r"B = \frac{1}{100}"),
+
+        )
